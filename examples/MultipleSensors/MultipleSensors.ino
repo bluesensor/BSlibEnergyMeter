5eceffa4c3b330b27de0eedba7ca3b6fe1949269
jhon-p16 <jhonpaulo1698@gmail.com>
2021-05-07 16:33:45 -0500

    refactor: cambio de metodos

--- examples/MultipleSensors/MultipleSensors.ino
+++ examples/MultipleSensors/MultipleSensors.ino
@@ -8,18 +8,16 @@
 
 #define CURRENT_FACTOR 0.200
 #define VOLTAGE_FACTOR 0.60
-#define BATTERY_FACTOR 0.20
-#define BATTERY_OFFSET 1.00
+#define VOLTAGE_OFFSET 0.60
 
 BslibEnergyMeter CurrentMeter;
 BslibEnergyMeter VoltageMeter;
-BslibEnergyMeter BatteryMeter;
 
 void setup()
 {
   CurrentMeter.SensorCurrent(PIN_CURRENT_REF, PIN_CURRENT, CURRENT_FACTOR);
-  VoltageMeter.SensorVoltage(PIN_VOLTAGE, VOLTAGE_FACTOR);
-  BatteryMeter.SensorBattery(PIN_BATTERY, BATTERY_FACTOR, BATTERY_OFFSET);
+  VoltageMeter.VoltageMeter(PIN_VOLTAGE, VOLTAGE_FACTOR, VOLTAGE_OFFSET);
+  // VoltageMeter.VoltageMeter(PIN_VOLTAGE, VOLTAGE_FACTOR); tmb puede ser sin offset
 }
 void loop()
 {
@@ -35,15 +33,11 @@ void loop()
   // ! la cantidad de lecturas debe ser configurada a discreción
 
   float corriente = CurrentMeter.GetCurrent(50);
-  float voltaje = VoltageMeter.GetVoltage(50);
-  float battery = BatteryMeter.GetBattery(50);
+  float voltaje = VoltageMeter1.GetVoltage(50);
 
-  Serial.print("Corriente en motor N del sistema:\t");
+  Serial.print("Corriente del sistema:\t");
   Serial.println(voltaje);
 
-  Serial.print("Voltaje en motor N del sistema:\t");
+  Serial.print("Voltaje del sistema:\t");
   Serial.println(corriente);
-
-  Serial.print("Nivel de bateria del sistema:\t");
-  Serial.println(battery);
 }
